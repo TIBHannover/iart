7bbc988b3ea386ae367d60ecfe6ea1d4ddbf40b4
bhaallord <demoniac.tux@gmail.com>
2021-12-14 08:57:28 +0100

    Extend installer to downloaded pretrained faiss indexes.

--- install.gpu.sh
+++ install.gpu.sh
@@ -3,4 +3,7 @@
 sudo chown -R 1000:1000 ./data/elasticsearch &&
 wget https://tib.eu/cloud/s/8j6QQoeHpjNtgtC/download/models_gpu.tar.gz &&
 tar -xf models_gpu.tar.gz --directory data &&
-rm models_gpu.tar.gz
+rm models_gpu.tar.gz &&
+wget https://tib.eu/cloud/s/2AosDSnKJnzoKe3/download/faiss.tar.gz &&
+tar -xf faiss.tar.gz --directory data &&
+rm faiss.tar.gz
--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env sh
 
 sudo chown -R 1000:1000 ./data/elasticsearch &&
+
 wget https://tib.eu/cloud/s/idRNbs6EJ5LKea6/download/models_cpu.tar.gz &&
 tar -xf models_cpu.tar.gz --directory data &&
-rm models_cpu.tar.gz
+rm models_cpu.tar.gz &&
+wget https://tib.eu/cloud/s/2AosDSnKJnzoKe3/download/faiss.tar.gz &&
+tar -xf faiss.tar.gz --directory data &&
+rm faiss.tar.gz 
